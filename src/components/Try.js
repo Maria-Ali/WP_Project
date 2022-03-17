f2d5ccf891be5b19e4c3ef8c706bef2942722d5f
Nimrabutt <nimb2018@gmail.com>
2022-03-17 09:55:19 +0500

    inital commit

---
+++ src/components/Try.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class Try extends Component{
+  render(){
+    return (
+      <div className="App">
+        <h1>Nimra and Maria's Project</h1>
+      </div>
+    );
+  }
+}
+
+export default Try;
