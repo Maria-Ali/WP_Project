defa04df75b2d984c5728d49ff255da7edb04962
Nimrabutt <nimb2018@gmail.com>
2022-04-12 20:38:23 +0500

    Login validation added.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import './Login.css';
+import '../css/Login.css';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -6,7 +6,6 @@ import Form from 'react-bootstrap/Form'
 import {Button, Container} from 'react-bootstrap';
 import Footer from '../components/Footer';
 import React, { useState } from 'react';
-import Feedback from 'react-bootstrap/Feedback';
 
 
 export default function Login(props) {
@@ -29,12 +28,15 @@ export default function Login(props) {
 
 
     const findFormErrors = () => {
-        const { email,password } = form
-        const newErrors = {}
+        const { email,password } = form;
+        const newErrors = {};
         // email errors
+        let a ='@';
+        // let emailC = email;
         if ( !email || email === '') newErrors.email = 'required';
-        else if(!email) newErrors.email = 'Incorrect Format';
-        
+        else if(!email.includes('@') ) {
+          newErrors.email = 'Incorrect Format';
+        }
         // password errors
         if ( !password || password === '' ) newErrors.password = 'cannot be blank!';
         else if ( password.length < 4 ) newErrors.password = 'password must be atleast 8 characters!';
@@ -50,7 +52,7 @@ export default function Login(props) {
     if ( Object.keys(newErrors).length > 0 ) {
       // We got errors!
       setErrors(newErrors)
-      console.log(errors.email);
+      // console.log(errors.email);
     } else {
       // No errors! Put any logic here for the form submission!
       alert('Thank you for your feedback!')
@@ -59,47 +61,48 @@ export default function Login(props) {
   return (
     <div className="back-container" style={{ 
         backgroundImage: `url(${process.env.PUBLIC_URL + '/images/bg.jpg'})` ,
-        // backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        // backgroundRepeat: 'no-repeat',
-
-      }}>
-          <Container>
+        backgroundSize: 'cover'}}>
+      <Container>
         <div className='login-container'>
+          
         <h1>Login</h1>
+        
             <Row>
-                <Col xs={5}>
-                    <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control required type="email" placeholder="Enter Email"
-                            onChange={e => setField('email',e.target.value)}
-                            isInvalid={!!errors.email}
-                        />
-                        <Form.Control.Feedback type='invalid'>
-                                { errors.email }
-                        </Form.Control.Feedback>
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control required type="password" placeholder="Password"
-                            onChange={e => setField('password',e.target.value)}
-                            isInvalid={!!errors.password}
-                        />
-                        <Form.Control.Feedback type='invalid'>
-                                { errors.password }
-                        </Form.Control.Feedback>
-                        </Form.Group>
-                        
-                        <Button variant="primary" type="submit" className='btn-submit'>
-                            Submit
-                        </Button>
-                        </Form>
-                </Col>
-                <Col><div className='hr'></div></Col>
-                <Col xs={5}>
-                    <div><img className='log-img img-fluid' src={process.env.PUBLIC_URL + '/images/s.jpg'} alt="login" /></div>
-                </Col>
+              <Col xs={6} className='col-2'>
+                  <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                          <Form.Label>Email</Form.Label>
+                          <Form.Control required type="email" placeholder="Enter Email"
+                          onChange={e => setField('email',e.target.value)}
+                          isInvalid={!!errors.email}
+                      />
+                      <Form.Control.Feedback type='invalid'>
+                              { errors.email }
+                      </Form.Control.Feedback>
+                      </Form.Group>
+                      <Form.Group className="mb-3" controlId="formBasicPassword">
+                          <Form.Label>Password</Form.Label>
+                          <Form.Control required type="password" placeholder="Password"
+                          onChange={e => setField('password',e.target.value)}
+                          isInvalid={!!errors.password}
+                      />
+                      <Form.Control.Feedback type='invalid'>
+                              { errors.password }
+                      </Form.Control.Feedback>
+                      </Form.Group>
+                      
+                      <Button variant="primary" type="submit" className='btn-submit'>
+                          Submit
+                      </Button>
+                      </Form>
+              </Col>
+              <Col xs={5} className='col-1'>
+                      <div><img className='log-img img-fluid' src='https://blush.design/api/download?shareUri=4EXm1oz--Ht_PfiB&c=Clothes_0%7Effbcbf_Hair_0%7Effe5b5_Skin_0%7Ead603f&w=800&h=800&fm=png' alt="login" /></div>
+                      {/* {process.env.PUBLIC_URL + '/images/s.jpg'} */}
+                      {/* <div><img className='log-img img-fluid' src={process.env.PUBLIC_URL + '/images/uncle1.png'} alt="login" /></div> */}
+              </Col>
+                {/* <Col><div className='hr'></div></Col> */}
+                
             </Row>
         </div>
         </Container>
